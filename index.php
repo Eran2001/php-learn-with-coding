32e2154d856aea24a4b0af3b3057e758426efa19
M-Eran-Hasareli <eran.hasarali.2001@gmail.com>
2025-11-27 20:24:52 +0530

    make some codes with time function

--- index.php
+++ index.php
@@ -8,4 +8,16 @@ function reqURI($link): bool
     return $_SERVER["REQUEST_URI"] === $link;
 }
 
+$date_array = getdate();
+
+foreach ($date_array as $key => $value) {
+    print "$key = $value\n";
+}
+
+$formatted_data = "Today's date: ";
+
+print $formatted_data;
+
+die();
+
 require "views/index.view.php";
