93042809c89c6e36971395740db0964c3a02672e
M-Eran-Hasareli <eran.hasarali.2001@gmail.com>
2025-11-28 11:29:27 +0530

    add a commit

--- index.php
+++ index.php
@@ -8,16 +8,4 @@ function reqURI($link): bool
     return $_SERVER["REQUEST_URI"] === $link;
 }
 
-$date_array = getdate();
-
-foreach ($date_array as $key => $value) {
-    print "$key = $value\n";
-}
-
-$formatted_data = "Today's date: ";
-
-print $formatted_data;
-
-die();
-
 require "views/index.view.php";
